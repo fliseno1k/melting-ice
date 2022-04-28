ad7222f7f624ea173af6854e450da0ded83240cd
Aleksei Flis <fliseno1k@gmail.com>
2022-04-28 17:44:04 +1000

    remove frontend folder from backend branch

--- frontend/src/services/config.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_PUBLIC_API,
-    headers: {
-        "Content-Type": "application/json;charset=UTF-8"
-    }
-});
